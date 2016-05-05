a25f7d35a8fc5c4eb55377b2d5dad88fc838843c
wilsonloo <wilsonloo21@163.com>
2016-05-05 13:46:04 +0800

    添加登录功能、创建文章给你

--- whome.go
+++ whome.go
@@ -13,6 +13,9 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
+
+	"gopkg.in/mgo.v2"
 )
 
 var g_global_cfg map[string]string
@@ -22,6 +25,7 @@ func dump_request(r *http.Request) {
 	fmt.Println("request map: ", r.Form)
 	fmt.Println("path: ", r.URL.Path)
 	fmt.Println("schema: ", r.URL.Scheme)
+	fmt.Println("Method", r.Method)
 	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
 		fmt.Println("key: ", k)
@@ -44,6 +48,7 @@ type Person struct {
 type Session struct {
 	ModuleDir    string
 	TopCommonTag string
+	IsAdmin      bool
 }
 
 var g_session Session
@@ -54,7 +59,7 @@ type TemplateEngine struct {
 func (this *TemplateEngine) Dispatch(w http.ResponseWriter, r *http.Request, path string) {
 
 	// parse top segment
-	top_content := EchoView("top.html")
+	top_content := EchoView("/top.html")
 
 	// get html file
 	body_content := EchoView(path)
@@ -89,7 +94,8 @@ func myServeHTTP(w http.ResponseWriter, r *http.Request) {
 		path = "/" + g_session.ModuleDir + "/lab.html"
 
 	} else {
-		os.Exit(3333)
+		http.Error(w, "Page not found "+path, 404)
+		return
 	}
 
 	gTemplateEngine.Dispatch(w, r, path)
@@ -110,6 +116,7 @@ func init_app_cfg() {
 	g_global_cfg["APP_ROOT_DIR"] = path_dir
 
 	g_session = Session{}
+	g_session.IsAdmin = false
 }
 
 func main() {
@@ -124,6 +131,9 @@ func main() {
 
 	fs_handler = http.FileServer(http.Dir(www_root + "/res/img/"))
 	http.Handle("/images/", http.StripPrefix("/images/", fs_handler))
+
+	http.HandleFunc("/login/", HandleLogin)
+	http.HandleFunc("/article/", HandleArticle)
 
 	fs_handler = http.FileServer(http.Dir(www_root + "/res/img/"))
 	http.Handle("/favicon.ico", http.StripPrefix("/favicon.ico", fs_handler))
@@ -187,6 +197,119 @@ func EchoView(view_name string, args ...interface{}) string {
 		ret_string = strings.Replace(ret_string, old, nested_string, -1)
 	}
 	return ret_string
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request, path string) {
+
+	// parse top segment
+	top_content := EchoView("/top.html")
+
+	// get html file
+	body_content := EchoView(path)
+
+	ret_content := top_content + body_content
+	io.WriteString(w, ret_content)
+}
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	submit := r.Form["wp-submit"]
+	if submit == nil {
+		Redirect(w, r, "/login/login.html")
+
+	} else {
+		user_name := r.Form["user_name"]
+		user_pwd := r.Form["user_name"]
+
+		if user_name == nil || len(user_name) < 1 || user_pwd == nil || len(user_pwd) < 1 {
+			http.Error(w, "user name or pwd not right ", 404)
+			return
+		}
+
+		// todo check password for user
+		if user_name[0] == "admin" && user_pwd[0] == "admin" {
+			g_session.IsAdmin = true
+			g_session.TopCommonTag = "id_top_common_tag_home"
+			Redirect(w, r, "/index.html")
+		}
+	}
+}
+
+func HandleArticle(w http.ResponseWriter, r *http.Request) {
+	if !g_session.IsAdmin {
+		http.Error(w, "Permition denied ", 404)
+		return
+	}
+
+	r.ParseForm()
+	if r.Form["op"] == nil {
+		// 仅仅显示
+		g_session.TopCommonTag = "id_top_common_tag_add_article"
+		Redirect(w, r, r.URL.Path)
+
+	} else {
+		op := r.Form["op"][0]
+		switch op {
+		case "add":
+			add_article(w, r)
+		default:
+			http.Error(w, "unknown op", 404)
+		}
+	}
+
+}
+
+type Article struct {
+	Title      string
+	Content    string
+	PostTime   string
+	ModifyTime string
+}
+
+func add_article(w http.ResponseWriter, r *http.Request) {
+	// todo add ariticle
+	if r.Form["title"] == nil {
+		http.Error(w, "invalid title or content", 404)
+		return
+	}
+
+	if r.Form["content"] == nil {
+		http.Error(w, "invalid title or content", 404)
+		return
+	}
+
+	title := r.Form["title"][0]
+	content := r.Form["content"][0]
+	if len(title) == 0 || len(content) == 0 {
+		http.Error(w, "invalid title or content", 404)
+		return
+	}
+
+	// todo call mongodb api
+	session := GetDBSession()
+	defer session.Clone()
+
+	c := session.DB("whome").C("article")
+	err := c.Insert(&Article{title, content, time.Now().String(), time.Now().String()})
+	if err != nil {
+		log.Fatalln("failed to insert db")
+		http.Error(w, "failed to insert db", 404)
+		return
+	}
+
+	// todo redirect to add-article-result page
+	g_session.TopCommonTag = "id_top_common_tag_lab"
+	Redirect(w, r, "/lab/lab.html")
+}
+
+func GetDBSession() *mgo.Session {
+	session, err := mgo.Dial("127.0.0.1")
+	CheckError(err)
+
+	session.SetMode(mgo.Monotonic, true)
+
+	return session
 }
 
 /*
